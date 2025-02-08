ba157edd300537b0a87a43fcfbca6fcc7ea69502
Tieli Cristine <tielicristinesilva@gmail.com>
2025-02-08 13:08:21 -0300

    first commit

---
+++ src/views/auth/Dashboard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import axiosInstance from '@/lib/axios.ts';
+
+const user = async () => {
+    try {
+        const response = await axiosInstance.get('/user');
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const logout = async () => {
+    try {
+        const response = await axiosInstance.get('/logout');
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+user();
+
+</script>
+<template>
+    <h1>Dash</h1>
+</template>
